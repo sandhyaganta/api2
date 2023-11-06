779b73f5b8ee8e7a528ec1028c3ac451e45a1ed3
sandhyaganta <gantasandhyavitasoft@gmail.com>
2023-11-06 11:05:41 +0530

    rooting path created section and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SectionComponent } from './section/section.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"section",
+    component:SectionComponent
+  },
+  {
+    path:"footer",
+    component:FooterComponent
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
